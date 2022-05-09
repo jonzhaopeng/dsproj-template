99f8bd2b3f22e663d550f158270e5e44c143afe1
jonzhaopeng <62473245+jonzhaopeng@users.noreply.github.com>
2022-05-09 10:51:20 +0800

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,13 +1,16 @@
-import os
-
-
-# TODO 以下Python3 命令应该修改为 subprocess.check_call 
-
-venv_pipeline = f"""
-python3 -m venv --system-site-packages .venv
-source .venv/bin/activate
-pip3 install -r requirements.txt
+"""
+配置项目下数据项目venv环境
 """
 
-# 配置项目下数据项目虚拟环境
-os.system(venv_pipeline)
+import os
+import sys
+import subprocess
+
+
+subprocess.check_call(
+    [sys.executable, '-m', 'venv', '--system-site-packages', '.venv'])
+
+os.system('source .venv/bin/activate')
+
+subprocess.check_call(
+    [sys.executable, '-m', 'pip', 'install', '-r', 'requirements.txt'])
