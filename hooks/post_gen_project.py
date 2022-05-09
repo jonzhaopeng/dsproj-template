f6b81345ac4ce154dc75d0f51002340fbfceabc9
jonzhaopeng <62473245+jonzhaopeng@users.noreply.github.com>
2022-05-09 10:31:16 +0800

    Update post_gen_project.py

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,10 @@
-python3 -m venv .venv
+import os
+
+venv_pipeline = f"""
+python3 -m venv --system-site-packages .venv
 source .venv/bin/activate
 pip3 install -r requirements.txt
+"""
 
+# 配置项目下数据项目虚拟环境
+os.system(venv_pipeline)
